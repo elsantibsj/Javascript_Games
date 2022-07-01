05ef56a0fd8936479b7ce02dcab44128fdb116d1
Santiago <elsantoposada@gmail.com>
2022-07-01 03:24:58 -0300

    Animacion de masticar

--- PacMan/index.js
+++ PacMan/index.js
@@ -31,20 +31,40 @@ class Player {
 		this.position = position;
 		this.velocity = velocity;
 		this.radius = 15;
+		this.radians = 0.75
+		this.openRate = 0.12
+		this.rotation = 0
 	}
 
 	draw(){
+		c.save()
+		c.translate(this.position.x, this.position.y)
+		c.rotate(this.rotation)
+		c.translate(-this.position.x, -this.position.y)
 		c.beginPath();
-		c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+		c.arc(
+		this.position.x, 
+		this.position.y, 
+		this.radius, 
+		this.radians,
+		Math.PI * 2 - this.radians
+		)
+
+		c.lineTo(this.position.x, this.position.y)
 		c.fillStyle = 'yellow';
 		c.fill();
 		c.closePath();
+		c.restore()
 	}
 
 	update(){
 		this.draw()
 		this.position.x += this.velocity.x
 		this.position.y += this.velocity.y
+
+		if (this.radians < 0 || this.radians > .75) this.openRate = -this.openRate
+
+			this.radians += this.openRate
 	}
 }
 
@@ -166,7 +186,7 @@ let score = 0
 
 const map = [
 ['1','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','2'],
-['|','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','|'],
+['|','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','p','|'],
 ['|','.','[','-','-','-','-','-','-',']','.','T','.','[','-','-','-','-','-','-',']','.','|'],
 ['|','.','.','.','.','.','.','.','.','.','.','|','.','.','.','.','.','.','.','.','.','.','|'],
 ['|','.','1','-','2','.','T','.','T','.','[','t',']','.','T','.','T','.','1','-','2','.','|'],
@@ -184,7 +204,7 @@ const map = [
 ['|','.','B','.','B','.','b','.','B','.','[','d',']','.','B','.','b','.','B','.','B','.','|'],
 ['|','.','.','.','.','.','.','.','.','.','.','|','.','.','.','.','.','.','.','.','.','.','|'],
 ['|','.','[','-','-','-','-','-','-',']','.','B','.','[','-','-','-','-','-','-',']','.','|'],
-['|','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','p','|'],
+['|','p','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.','p','|'],
 ['4','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','3']
 ]
 
@@ -675,6 +695,11 @@ player.update()
 		}
 
 	})
+
+	if (player.velocity.x > 0) player.rotation = 0
+	else if (player.velocity.x < 0) player.rotation = Math.PI
+	else if (player.velocity.y > 0) player.rotation = Math.PI / 2
+	else if (player.velocity.y < 0) player.rotation = Math.PI * 1.5
 }
 
 animate()
